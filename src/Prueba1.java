dc14ac077eb29dd1f16c4c2cc0cc6c4f7ffb19ad
Ñoño <afermar860n@g.educaand.es>
2023-10-19 21:45:49 +0200

    pruebaCasa2

--- src/Prueba1.java
+++
@@ -1,8 +0,0 @@
-import java.out.Scanner;
-
-public class Prueba1 {
-    public static void main(String[] args) {
-
-
-    }
-}
---
+++ src/promedio.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class promedio {
+    public static void main(String[] args) {
+        System.out.println("escribe primera nota:");
+
+    }
+}
