537fe1b313b1394795a9deccdcf4f4ac44ba4dbb
afermar860n <146812421+afermar860n@users.noreply.github.com>
2023-10-24 20:24:11 +0200

    pruebaCasa2

---
+++ src/Ciclos2.java
@@ -0,0 +1,53 @@
+public class Ciclos2 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int j = 99;
+
+        System.out.println("Serie con for:");
+        for (i = 1; i <= 5; i++){
+            if (i < 5) {
+                System.out.print(i + ", ");
+                System.out.print(j + ", ");
+            }else {
+                System.out.print(i + ", ");
+                System.out.print(j);
+            }
+         j--; // decrecimiento de J dentro del for
+        }
+    System.out.println(""); //espacio
+
+        i = 1;
+        j = 99;
+
+        System.out.println("Serie con while: ");
+        while (i <= 5){
+            if (i < 5) {
+                System.out.print(i + ", ");
+                System.out.print(j + ", ");
+            }else {
+                System.out.print(i + ", ");
+                System.out.print(j);
+            }
+        i++;  //crecimiento de i en el while
+        j--;  //decrecimiento de jota
+        }
+    System.out.println(""); //espacio
+
+        i = 1;
+        j = 99;
+
+        System.out.println("Serie con do-while: ");
+        do {
+            if (i < 5) {
+                System.out.print(i + ", ");
+                System.out.print(j + ", ");
+            }else {
+                System.out.print(i + ", ");
+                System.out.print(j);
+            }
+            i++;  //crecimiento de i en el while
+            j--;  //decrecimiento de jota
+        }while(i <= 5);
+    }
+}
---
+++ src/ForWhileDoWhile.java
@@ -0,0 +1,45 @@
+public class ForWhileDoWhile {
+    public static void main(String[] args) {
+
+
+        int i = 0;
+        System.out.println(""); //print para dejar espacio
+
+        System.out.println("Serie con for + if,else:");
+        for (i = 1; i <=10; i++){
+            if (i < 10) {      //Este "if,else" es para imprimir la coma y no imprimirla cuando el ciclo llegue a 10
+                System.out.print(i + ", ");
+            }else {
+               System.out.print(i);
+            }
+        }
+
+
+        i = 1;
+        System.out.println(""); //print para dejar espacio
+
+        System.out.println("Serie con while + if,else:");
+        while (i <= 10){
+            if (i < 10) {      //Este "if,else" es para imprimir la coma y no imprimirla cuando el ciclo llegue a 10
+                System.out.print(i + ", ");
+            }else {
+                System.out.print(i);
+            }
+        i++; // En el while el aumento se pone al final
+        }
+
+
+        i = 1;
+        System.out.println(""); //print para dejar espacio
+
+        System.out.println("Serie con do/while + if,else:");
+        do{
+            if (i < 10) {      //Este "if,else" es para imprimir la coma y no imprimirla cuando el ciclo llegue a 10
+                System.out.print(i + ", ");
+            }else {
+                System.out.print(i);
+            }
+        i++; // En el do el aumento se pone al final
+        }while (i <= 10);
+    }
+}
